2444acdad4489f246394447f703d2f8956bbc178
marcpetitvecino <marc.petit@mataro.epiaedu.cat>
2020-10-07 20:43:22 +0200

    Little optimization

--- app/src/main/java/com/example/calculadorapasiona/MainActivity.kt
+++ app/src/main/java/com/example/calculadorapasiona/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.core.content.ContextCompat
 import java.lang.Exception
 import java.math.BigDecimal
 import java.math.RoundingMode
+private const val DOLAR = "dolar"
+private const val LLIURA = "lliura"
+private const val YEN = "yen"
+private const val YUAN = "yuan"
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var button0: Button
     private lateinit var button1: Button
     private lateinit var button2: Button
@@ -40,12 +43,15 @@ class MainActivity : AppCompatActivity() {
     private var conversioYuan: Double = 1.0
     private var conversioActual: Double = 1.0
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
         initListeners()
+        resetCalculator()
+    }
+
+    private fun resetCalculator() {
         input.text = "0"
         output.text = "0"
     }
@@ -74,193 +80,167 @@ class MainActivity : AppCompatActivity() {
         buttonYuan = findViewById(R.id.btnYuan)
         pasiona = findViewById(R.id.pasiona)
     }
-
     private fun initListeners() {
-        button0.setOnClickListener {
-            addNumero(0)
-        }
-        button1.setOnClickListener {
-            addNumero(1)
-        }
-        button2.setOnClickListener {
-            addNumero(2)
-        }
-        button3.setOnClickListener {
-            addNumero(3)
-        }
-        button4.setOnClickListener {
-            addNumero(4)
-        }
-        button5.setOnClickListener {
-            addNumero(5)
-        }
-        button6.setOnClickListener {
-            addNumero(6)
-        }
-        button7.setOnClickListener {
-            addNumero(7)
-        }
-        button8.setOnClickListener {
-            addNumero(8)
-        }
-        button9.setOnClickListener {
-            addNumero(9)
-        }
-        buttonCE.setOnClickListener {
-            input.text = "0"
-            output.text = "0"
-        }
-        buttonDel.setOnClickListener {
-            if (input.length() != 1)input.text = input.text.substring(0, input.length()-1) else input.text = "0"
-        }
-        buttonComa.setOnClickListener {
-             if (!checkComa()){
-                 input.text = "${input.text},"
-             }
-        }
-        buttonIgual.setOnClickListener {
-            if (conversioActual == 1.0) {
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("Alerta!")
-                builder.setMessage("No has escollit cap moneda per fer la conversió")
-                val dialog = builder.create()
-                dialog.show()
-            } else {
-                val comaToPoint = input.text.toString().replace(',','.')
-                var resultat = comaToPoint.toDouble()*conversioActual
-                if (resultat % 1.0 != 0.0) {
-                    output.text = truncate(resultat.toString(), 2)!!.replace('.',',')
-                } else output.text = resultat.toInt().toString()
-            }
-        }
-        buttonDolar.setOnClickListener {
 
-            if (conversioDolar == 1.0) {
-                val builder = AlertDialog.Builder(this)
-                val editText = EditText(this)
-                val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT)
-                editText.layoutParams = lp
-                editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
-                builder.setTitle("Introdueix el valor de conversió del dòlar")
-                builder.setView(editText)
-                builder.setPositiveButton("Aceptar") { position, which ->
-                    
-                        try {
-                            conversioDolar = editText.text.toString().toDouble()
-                            conversioActual = conversioDolar
-                        } catch (e: Exception) {
-                            Toast.makeText(this, "Input no vàlid", Toast.LENGTH_SHORT).show()
-                        }
+        button0.setOnClickListener { addNumero(0) }
+        button1.setOnClickListener { addNumero(1) }
+        button2.setOnClickListener { addNumero(2) }
+        button3.setOnClickListener { addNumero(3) }
+        button4.setOnClickListener { addNumero(4) }
+        button5.setOnClickListener { addNumero(5) }
+        button6.setOnClickListener { addNumero(6) }
+        button7.setOnClickListener { addNumero(7) }
+        button8.setOnClickListener { addNumero(8) }
+        button9.setOnClickListener { addNumero(9) }
+        buttonCE.setOnClickListener { resetCalculator() }
+        buttonDel.setOnClickListener { if (input.length() != 1)input.text = input.text.substring(0, input.length()-1) else input.text = "0" }
+        buttonComa.setOnClickListener { if (!checkComa()){ input.text = "${input.text}," } }
+        buttonIgual.setOnClickListener { igualar() }
+        buttonDolar.setOnClickListener { setDolarListener() }
+        buttonLliura.setOnClickListener { setLliuraListener() }
+        buttonYen.setOnClickListener { setYenListener() }
+        buttonYuan.setOnClickListener { setYuanListener() }
+        pasiona.setOnClickListener { Toast.makeText(this, "Benvingut a Pasiona! La millor empresa del mòn", Toast.LENGTH_SHORT).show() }
+    }
 
+    private fun setYuanListener() {
+
+        if (conversioYuan == 1.0) {
+            val builder = AlertDialog.Builder(this)
+            val editText = EditText(this)
+            val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT)
+            editText.layoutParams = lp
+            editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+            builder.setTitle("Introdueix el valor de conversió del yuan")
+            builder.setView(editText)
+            builder.setPositiveButton("Aceptar") { position, which ->
+                try {
+                    conversioYuan = editText.text.toString().toDouble()
+                    conversioActual = conversioYuan
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Input no vàlid", Toast.LENGTH_SHORT).show()
                 }
-                builder.setNegativeButton("Cancelar", null)
-                val dialog = builder.create()
-                dialog.show()
-            } else {
-                conversioActual = conversioDolar
             }
-
-            buttonDolar.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark))
-            buttonLliura.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonYen.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonYuan.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+            builder.setNegativeButton("Cancelar", null)
+            val dialog = builder.create()
+            dialog.show()
+        } else {
+            conversioActual = conversioYuan
         }
-        buttonLliura.setOnClickListener {
+        highlightSelected(YUAN)
+    }
 
-            if (conversioLliura == 1.0) {
-                val builder = AlertDialog.Builder(this)
-                val editText = EditText(this)
-                val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT)
-                editText.layoutParams = lp
-                editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
-                builder.setTitle("Introdueix el valor de conversió de la lliura")
-                builder.setView(editText)
-                builder.setPositiveButton("Aceptar") { position, which ->
-                    try {
-                        conversioLliura = editText.text.toString().toDouble()
-                        conversioActual = conversioLliura
-                    } catch (e: Exception) {
-                        Toast.makeText(this, "Input no vàlid", Toast.LENGTH_SHORT).show()
-                    }
+    private fun setYenListener() {
+        if (conversioYen == 1.0) {
+            val builder = AlertDialog.Builder(this)
+            val editText = EditText(this)
+            val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            editText.layoutParams = lp
+            editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+            builder.setTitle("Introdueix el valor de conversió del yen")
+            builder.setView(editText)
+            builder.setPositiveButton("Aceptar") { position, which ->
+                try {
+                    conversioYen = editText.text.toString().toDouble()
+                    conversioActual = conversioYen
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Input no vàlid", Toast.LENGTH_SHORT).show()
                 }
-                builder.setNegativeButton("Cancelar", null)
-                val dialog = builder.create()
-                dialog.show()
-            } else {
-                conversioActual = conversioLliura
             }
-
-            buttonDolar.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonLliura.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark))
-            buttonYen.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonYuan.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+            builder.setNegativeButton("Cancelar", null)
+            val dialog = builder.create()
+            dialog.show()
+        } else {
+            conversioActual = conversioYen
         }
-        buttonYen.setOnClickListener {
-            if (conversioYen == 1.0) {
-                val builder = AlertDialog.Builder(this)
-                val editText = EditText(this)
-                val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT)
-                editText.layoutParams = lp
-                editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
-                builder.setTitle("Introdueix el valor de conversió del yen")
-                builder.setView(editText)
-                builder.setPositiveButton("Aceptar") { position, which ->
-                    try {
-                        conversioYen = editText.text.toString().toDouble()
-                        conversioActual = conversioYen
-                    } catch (e: Exception) {
-                        Toast.makeText(this, "Input no vàlid", Toast.LENGTH_SHORT).show()
-                    }
+        highlightSelected(YEN)
+    }
+
+    private fun setLliuraListener() {
+        if (conversioLliura == 1.0) {
+            val builder = AlertDialog.Builder(this)
+            val editText = EditText(this)
+            val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT)
+            editText.layoutParams = lp
+            editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+            builder.setTitle("Introdueix el valor de conversió de la lliura")
+            builder.setView(editText)
+            builder.setPositiveButton("Aceptar") { position, which ->
+                try {
+                    conversioLliura = editText.text.toString().toDouble()
+                    conversioActual = conversioLliura
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Input no vàlid", Toast.LENGTH_SHORT).show()
                 }
-                builder.setNegativeButton("Cancelar", null)
-                val dialog = builder.create()
-                dialog.show()
-            } else {
-                conversioActual = conversioYen
             }
-
-            buttonDolar.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonLliura.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonYen.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark))
-            buttonYuan.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+            builder.setNegativeButton("Cancelar", null)
+            val dialog = builder.create()
+            dialog.show()
+        } else {
+            conversioActual = conversioLliura
         }
-        buttonYuan.setOnClickListener {
-            if (conversioYuan == 1.0) {
-                val builder = AlertDialog.Builder(this)
-                val editText = EditText(this)
-                val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT)
-                editText.layoutParams = lp
-                editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
-                builder.setTitle("Introdueix el valor de conversió del yuan")
-                builder.setView(editText)
-                builder.setPositiveButton("Aceptar") { position, which ->
-                    try {
-                        conversioYuan = editText.text.toString().toDouble()
-                        conversioActual = conversioYuan
-                    } catch (e: Exception) {
-                        Toast.makeText(this, "Input no vàlid", Toast.LENGTH_SHORT).show()
-                    }
+        highlightSelected(LLIURA)
+    }
+
+    private fun setDolarListener() {
+        if (conversioDolar == 1.0) {
+            val builder = AlertDialog.Builder(this)
+            val editText = EditText(this)
+            val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT)
+            editText.layoutParams = lp
+            editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+            builder.setTitle("Introdueix el valor de conversió del dòlar")
+            builder.setView(editText)
+            builder.setPositiveButton("Aceptar") { position, which ->
+                try {
+                    conversioDolar = editText.text.toString().toDouble()
+                    conversioActual = conversioDolar
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Input no vàlid", Toast.LENGTH_SHORT).show()
                 }
-                builder.setNegativeButton("Cancelar", null)
-                val dialog = builder.create()
-                dialog.show()
-            } else {
-                conversioActual = conversioYuan
+
             }
-
-            buttonDolar.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonLliura.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonYen.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
-            buttonYuan.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark))
+            builder.setNegativeButton("Cancelar", null)
+            val dialog = builder.create()
+            dialog.show()
+        } else {
+            conversioActual = conversioDolar
         }
+        highlightSelected(DOLAR)
+    }
 
-        pasiona.setOnClickListener {
-            Toast.makeText(this, "Benvingut a Pasiona! La millor empresa del mòn", Toast.LENGTH_SHORT).show()
+    private fun igualar() {
+        if (conversioActual == 1.0) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Alerta!")
+            builder.setMessage("No has escollit cap moneda per fer la conversió")
+            val dialog = builder.create()
+            dialog.show()
+        } else {
+            val comaToPoint = input.text.toString().replace(',','.')
+            var resultat = comaToPoint.toDouble()*conversioActual
+            if (resultat % 1.0 != 0.0) {
+                output.text = truncate(resultat.toString(), 2)!!.replace('.',',')
+            } else output.text = resultat.toInt().toString()
         }
+    }
+
+    private fun highlightSelected(divisa: String) {
+        buttonDolar.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+        buttonLliura.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+        buttonYen.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+        buttonYuan.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+        val escollit = when(divisa) {
+            DOLAR -> buttonDolar
+            LLIURA -> buttonLliura
+            YEN -> buttonYen
+            YUAN -> buttonYuan
+            else -> return
+        }
+        escollit.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_blue_dark))
     }
 
     private fun addNumero(numero: Int) {
@@ -283,4 +263,5 @@ class MainActivity : AppCompatActivity() {
     private fun checkComa(): Boolean {
         return input.text.contains(",")
     }
+
 }
